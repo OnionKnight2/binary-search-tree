3223a827f61b6099668011a17d1ea71c22a5897c
rastko <vucen.rastko@gmail.com>
2023-05-03 00:08:31 +0200

    Complete #insert(value) method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -19,6 +19,23 @@ class Tree
     root_node
   end
 
+  def insert(value, root_node = @root)
+    # If a accepted value already exist, return nil
+    return nil if value == root_node.data
+
+    if value < root_node.data
+      # Traverse the left subtree
+      root_node.left.nil? ? root_node.left = Node.new(value) : insert(value, root_node.left)
+    else
+      # Traverse the right subtree
+      root_node.right.nil? ? root_node.right = Node.new(value) : insert(value, root_node.right)
+    end
+  end
+
+  def delete(value, root_node = @root)
+    
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -42,4 +59,8 @@ class Node
 end
 
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.pretty_print
+tree.insert(123)
+tree.insert(0)
+tree.insert(2132)
 tree.pretty_print
